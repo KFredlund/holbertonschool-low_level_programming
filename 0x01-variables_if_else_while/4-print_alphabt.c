9b0cda9f5ea67cca45c904a12c3577a15e2ee229
KFredlund <katihomes@gmail.com>
2019-09-19 17:48:16 +0000

    use != in a for loop to exclude q and e from printing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* x - stores alphabet value currently being parsed
+* n - stores new line character
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+	char n = "\n"
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != e && x != q)
+		{
+			putchar(x);
+		}
+	}
+	putchar(n);
+	return (0);
+}
