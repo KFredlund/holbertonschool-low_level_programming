8bf68fd22c2ad1f890f17f4a80e71289977c45a1
KFredlund <katihomes@gmail.com>
2019-09-19 17:58:51 +0000

    Using a for loop to parse throug numbers 0-9, and printf to print to screen along with new line character

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* x - stores the value of the number being parsed
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
