db5a1404b2e8559b8569f097b6dbb0ebed7fcbca
KFredlund <katihomes@gmail.com>
2019-09-23 19:42:30 +0000

    a function that prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_aplhabet - function that prints alphabet from a-z
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
