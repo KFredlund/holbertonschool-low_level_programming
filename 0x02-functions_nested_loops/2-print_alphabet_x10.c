0381f23667ea675ba7d340386b3fac3994d57e8f
KFredlund <katihomes@gmail.com>
2019-09-23 19:56:20 +0000

    a function that prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - function to print the alphabet ten times
+*
+*/
+void print_alphabet_x10(void)
+{
+	int round;
+	int letter;
+
+	for (round = 0; round <= 10; round++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
