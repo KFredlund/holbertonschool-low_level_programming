717b18a643347463c830226d02b5d697c269c985
KFredlund <katihomes@gmail.com>
2019-09-24 14:24:57 +0000

    properly document variables for betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
 * print_last_digit - function that prints the last digit of a number
-* num - stores value for function to act upon
+* @num: stores value for function to act upon
+* @lastDig: stores value of last digit
 * Return: returns the last value of the digit
 */
 int print_last_digit(int num)
