7dfa856e4d059ba0abf30e574a261d7632b06727
KFredlund <katihomes@gmail.com>
2019-09-24 18:46:44 +0000

    remove unnecessary inputs in _putchar to print chars and simplify code by reducing putchars to outside of if/else statement

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,40 +1,28 @@
 #include "holberton.h"
 /**
 * print_sign - prints the sign of a number
-*
+* @n: first integer
 * Return: 1 if n is greater than 0, 0 if its 0, -1 if n is less than 0
 */
 int print_sign(int n)
 {
-	int plus = '+';
-	int zero = '0';
-	int minus = '-';
-
 	if (n > 0)
 	{
-		return (1);
-		_putchar(plus);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n);
-		_putchar('\n');
+		_putchar('+');
+		return(1);
 	}
 	else if (n == 0)
 	{
+		_putchar('0');
 		return (0);
-		_putchar(zero);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n);
-		_putchar('\n');
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
-		_putchar(minus);
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n);
-		_putchar('\n');
 	}
+	_putchar(',');
+	_putchar(' ');
+	_putchar(n);
+	_putchar('\n');
 }
