aea29b4971c349165ddb5e7192255184aba7f3d4
KFredlund <katihomes@gmail.com>
2019-09-24 14:21:40 +0000

    Properly document the var name as @num: description

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 *  _abs - function that computes the absolute value of an integer
-* num - integer variable
+* @num: integer variable
 * Return: returns value of integer
 */
 int _abs(int num)
