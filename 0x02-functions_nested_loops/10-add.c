70c58634ae78a236e912246e2d923faf65ece563
KFredlund <katihomes@gmail.com>
2019-09-24 14:47:36 +0000

    remove putchar as we are not printing anything, just return sum of a and b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* add - function that adds two integers
+* @a: first integer
+* @b: second integer
+* Return: result
+*/
+int add(int a, int b)
+{
+	int sum = (a + b);
+
+	return (sum);
+}
