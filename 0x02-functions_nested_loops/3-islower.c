a05549400c0c8fd64393f83ae3c9ef9be66ce5cd
KFredlund <katihomes@gmail.com>
2019-09-24 14:17:39 +0000

    properly declare variable in docs as @varName: <description>

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 * _islower -  checks for lowercase character
-* c - integer which holds value to act upon
+* @c: int variable which holds value to act upon
 * Return: 1 if c is lowercarse,
 */
 int _islower(int c)
