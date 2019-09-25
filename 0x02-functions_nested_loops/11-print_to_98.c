dca1e4cafdb5eb98a416ef7ae6be8d7398596005
KFredlund <katihomes@gmail.com>
2019-09-25 00:24:40 +0000

    document int n for betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
 * print_to_98 - function that prints all natural numbers from n to 98
-*
+* @n: holds number given in main function
 * Return: Always 0 (Success)
 */
 void print_to_98(int n)
-{	
+{
 	int i;
 
 	for (i = 0; i <= n; i++)
