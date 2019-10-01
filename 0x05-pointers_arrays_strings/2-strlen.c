6e9539f674c08b88acbfaa1977e56ae0b63f8eb8
KFredlund <katihomes@gmail.com>
2019-10-01 12:40:23 +0000

    a function that prints the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,14 @@
 /**
 * _strlen - function that returns the length of a string
 * @s: first integer
-*
 * Return: Length of a string
 */
 int _strlen(char *s)
 {
-	int n = 0;
+        int n = 0;
 
-	while (s[n] != '\0')
-		n++;
+        while (s[n] != '\0')
+                n++;
 
-	return (n);
+        return (n);
 }
