596a2d3491b40115adef7c223cd73ef69fd889ca
KFredlund <katihomes@gmail.com>
2019-10-02 01:06:20 +0000

    Fix indentation issues

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_rev - function that prints a string in reverse
+* @s: first integer
+* Return: Always 0
+*/
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		 _putchar(s[i]);
+	}
+	_putchar('\n');
+}
