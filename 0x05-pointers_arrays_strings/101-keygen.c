e2ac3de8d64d538d9df586df5e514ab1fe9280cc
KFredlund <katihomes@gmail.com>
2019-10-02 03:21:20 +0000

    function to generate random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* passgen - function to create a random password
+*
+*
+*/
+void create_pass( char* randomString, size_t length )
+{
+    sizet_t i = 0;
+
+    srand(rand());
+    strcpy(randomString,"");
+
+    for( i = 0; i < (length/2); i++ )
+    {
+        *(randomString + i) = ( rand() % 25 + 65 );
+    }
+    for( ; i < length; i++ )
+    {
+        *(randomString + i) = ( rand() % 9 + 48 );
+    }
+} 
