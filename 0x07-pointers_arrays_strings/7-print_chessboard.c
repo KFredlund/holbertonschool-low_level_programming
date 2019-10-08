1c2bf21913fd5f0cac290f1dca011a70986209c2
KFredlund <katihomes@gmail.com>
2019-10-08 18:01:52 +0000

    Function that prints a chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,34 +1,19 @@
 #include "holberton.h"
-#define NULL 0
 /**
 * print_chessboard - function that prints the chessboard
-* @a: pointer array
+* @a: first integer
 * Return: Always 0
 */
-char *_strcpy(char *dest, char *src);
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < *a[i]; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
-}
-
-char *_strcpy(char *dest, char *src)
- {
- 	char *strres = dest;
-
- 	if ((dest != NULL) && (src != NULL))
- 	{
- 		while (*src != NULL)
- 		{
- 			*dest++ = *src++;
- 		}
- 		*dest = NULL;
- 	}
- 	return (strres);
 }
