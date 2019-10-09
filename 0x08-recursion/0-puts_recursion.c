598469785d634ed983126fb5ff65d737b3c51685
KFredlund <katihomes@gmail.com>
2019-10-09 16:02:23 +0000

    Add if statement to look for null byte to add new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,12 @@
 */
 void _puts_recursion(char *s)
 {
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	if (*s == '\0')
-		return;
-	_putchar(*s);
-	_puts_recursion(++s);
-	_putchar('\n');
-}
+		_putchar('\n');
+	return;
+}	
