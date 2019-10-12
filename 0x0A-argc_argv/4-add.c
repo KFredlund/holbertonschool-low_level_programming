df3ae393be7b9d59b0179447e1b56790dee1df8d
KFredlund <katihomes@gmail.com>
2019-10-12 01:15:16 +0000

    program that adds positive numbers, accounting for non integer inputs

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/* int is_dig(char s[]); */
+/**
+* main - Entry point
+* @argc: counts pointers in array argv
+* @argv: array of pointers
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	sum = 0;
+	for (a = 1; a < argc; a++)
+	{
+		sum += atoi(argv[a]);
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("ERROR\n");
+				return (0);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
