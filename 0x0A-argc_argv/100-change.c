e0f94fbd1b3bc2f3da66c3a291f65acd2b995f75
KFredlund <katihomes@gmail.com>
2019-10-13 15:46:07 +0000

    Function that prints min number of cents you need to give back

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+* main - Entry point
+* @argc: counts pointers in array argv
+* @argv: array of pointers
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, coins;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	coins = 0;
+	for (a = 1; a < argc; a++)
+	{
+		coins += atoi(argv[a]);
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (atoi(argv[b]) < 0)
+			{
+				printf("0\n");
+				return (0);
+			}
+		}
+	}
+	printf("%d\n", coins);
+	return (0);
+}
