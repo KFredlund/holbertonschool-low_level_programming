3e8cacc05e25b4d4a86c1419075d6ee575885d55
KFredlund <katihomes@gmail.com>
2019-10-14 16:30:57 +0000

    Add for loop to acount for spaces

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,19 +6,19 @@
 */
 void print_diagonal(int n)
 {
-	int rows;
+	int rows, cols, col2;
 
-	for (; n > 0; n--)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		if (n > 0)
+		for (rows = 1; rows <= n; rows++)
 		{
-			for (rows = 0; rows <= n; rows++)
-			{
+			for (cols = 1; cols <= rows; cols++)
 				_putchar(' ');
-			}
-			_putchar('\\');
+			for (col2 = 1; col2 <= 1; col2++)
+				_putchar('\\');	
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
