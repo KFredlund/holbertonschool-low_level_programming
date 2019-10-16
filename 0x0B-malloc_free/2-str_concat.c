d23934721553271a8e0f4efd5390a1467d9f046d
KFredlund <katihomes@gmail.com>
2019-10-16 18:01:24 +0000

    Add k + n in 4th loop

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,28 +2,31 @@
 #include <stdlib.h>
 /**
  * str_concat - Function that concatenates
- * two strings 
+ * two strings
  * @s1: first char var
  * @s2: second char var
  * Return: Null on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j;
+	int i, j, k, n;
 	char *both;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\n'; j++)
 		;
+	j++;
 	both = malloc((i + j + 1) * sizeof(char));
-	if(both == NULL)
+	if (both == NULL)
 		return (NULL);
-	both[i] = '\n';
-	free(both);
+	for (k = 0; k < i; k++)
+		both[k] = s1[k];
+	for (n = 0; n < j; n++)
+		both[n + k] = s2[n];
 	return (both);
 }
