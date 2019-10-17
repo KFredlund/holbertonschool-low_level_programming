b6674b80048945d96950f5eb5a28633ce3785869
KFredlund <katihomes@gmail.com>
2019-10-17 18:44:02 +0000

    Use void pointer to zero out string

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,15 +9,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *str;
+	void *str;
 	unsigned int i;
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
-	str = malloc(nmemb * size);
+	str = (char *)malloc(nmemb * size);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		str[i] = 0;
+		*(char *)str = 0;
 	return (str);
 }
