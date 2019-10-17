b3cfcb78aceb6bb8b2fd847037781d4394d5066b
KFredlund <katihomes@gmail.com>
2019-10-17 19:08:04 +0000

    Add parenthesis to cast in arithmetic statement

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,6 +18,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
-		*(char *)str = 0;
+		*((char *)str + i) = 0;
 	return (str);
 }
