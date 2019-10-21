467448ba210f6242aa806d2286698edbaea93441
KFredlund <katihomes@gmail.com>
2019-10-21 21:18:42 +0000

    use a for loop to store copies of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,19 +11,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
-	int len1 = 0, len2;
+	int len1, len2, i, j;
 
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-	while (name[len1] != '\0')
-		len1++;
+	for (len1 = 0; name[len1] != '\0'; len1++)
+		;
 	new->name = malloc(sizeof(len1));
-	new->name = name;
+	for (i = 0; name[i] != '\0'; i++)
+		new->name[i] = name[i];
 	new->age = age;
 	for (len2 = 0; owner[len2]; len2++)
 		;
 	new->owner = malloc(sizeof(len2));
-	new->owner = owner;
+	for (j = 0; owner[j] != '\0'; j++)
+		new->owner[j] = owner[j];
 	return (new);
 }
