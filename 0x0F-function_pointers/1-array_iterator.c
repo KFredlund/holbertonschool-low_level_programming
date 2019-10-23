1886a30ea4a4e0a04de864238f0937ac8b64587f
KFredlund <katihomes@gmail.com>
2019-10-23 16:51:39 +0000

    Use size_t type for incrementer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,8 +9,8 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
 	for (i = 0; i < size; i++)
-		(*action)(array[i]);
+		(action)(array[i]);
 }
