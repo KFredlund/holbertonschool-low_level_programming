843172416e615b2d4da1d202efbe117e315579a7
KFredlund <katihomes@gmail.com>
2019-10-31 20:17:38 +0000

    Use write to print

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,21 +1,14 @@
-	extern printf
+          global    _start
 
-	section .data
-msg:	db 	"Hello, Holberton", 0
-fmt:	db	"%s", 10, 0
+          section   .text
+_start:   mov       rax, 1                  ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 60                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-	section	.text
-
-	global	main
-main:
-	push	rbp
-
-	mov	rdi,fmt
-	mov	rsi,msg
-	mov	rax,0
-	call	printf
-
-	pop	rbp
-
-	mov	rax,0
-	ret
+          section   .data
+message:  db        "Hello, Holberton", 10  ; note the newline at the end
