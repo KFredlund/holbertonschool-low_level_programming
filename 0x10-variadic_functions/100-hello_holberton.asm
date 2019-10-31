860147df08b724b5846845422c868e545bb1def0
KFredlund <katihomes@gmail.com>
2019-10-31 20:32:53 +0000

    Correct message length

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,15 @@
-          global    _start
+	global	_start
 
-          section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+	section	.text
+_start:	mov	rax, 1                  ; system call for write
+	mov	rdi, 1                  ; file handle 1 is stdout
+	mov	rsi, message            ; address of string to output
+	mov	rdx, 17                 ; number of bytes
+	syscall                          ; invoke operating system to do the write
 
-          section   .data
+	mov	rax, 60
+	mov	rdi, 0                 ; system call for exit
+	syscall                          ; invoke operating system to exit
+
+	section	.rodata
 message:  db        "Hello, Holberton", 10  ; note the newline at the end
