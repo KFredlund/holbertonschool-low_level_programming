c68b0605c6ee5ddffd5095ed4424103d991357f8
KFredlund <katihomes@gmail.com>
2019-10-31 20:03:57 +0000

    Call printf to print

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,21 @@
-global	main
+	extern printf
+
+	section .data
+msg:	db 	"Hello, Holberton", 0
+fmt:	db	"%s", 10, 0
 
 	section	.text
-_start:	mov	rax, 1		; write
-	mov	rdi, 1		; STDOUT_FILENO,
-	mov 	rsi, message	; Address of string to output
-	mov 	rdx, 17		; Number of bytes
-	syscall			; invoke os to do the write
-	mov 	rax, 60		; system call for exit
-	xor 	rdi, rdi	; exit code 0
-	syscall			; invoke os to exit
 
-	section .rodata
-messag:	db 	"Hello, Holberton", 10
+	global	main
+main:
+	push	rbp
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+
+	pop	rbp
+
+	mov	rax,0
+	ret
