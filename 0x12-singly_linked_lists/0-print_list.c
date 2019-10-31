58a2ec8359b61d2984cb93e6b5460c47811470c4
KFredlund <katihomes@gmail.com>
2019-10-31 20:54:24 +0000

    Move counter to outside of loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,15 +14,13 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
-			count++;
-			h = h->next;
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			count++;
-			h = h->next;
 		}
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
