78a77b558d7ca383d806b4cf3c4887ce4a3389a3
KFredlund <katihomes@gmail.com>
2019-11-01 14:19:21 +0000

    Account for length with a for loop

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,12 +10,20 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
+	int i = 0;
 	list_t *temp;
 	char *str2 = strdup(str);
 
+	while (str[i++])
+		;
 	temp = malloc(sizeof(list_t));
-	temp->str = str2;
-	temp->next = *head;
-	*head = temp;
-	return (*head);
+	if (str)
+	{
+		temp->str = str2;
+		temp->len = i - 1;
+		temp->next = *head;
+		*head = temp;
+		return (temp);
+	}
+	return (NULL);
 }
