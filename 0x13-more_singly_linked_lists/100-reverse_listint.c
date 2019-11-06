747b36a39642d69085acf3015a0c414778768b86
KFredlund <katihomes@gmail.com>
2019-11-06 03:48:02 +0000

    Remove unnecessary variable

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,16 +7,22 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *previous =  NULL;
-	listint_t *next;
+	listint_t *current;
+	listint_t *previous;
 
-	while (current)
+	if (*head != NULL)
 	{
-		next = current->next;
+		previous = *head;
+		current = (*head)->next;
+		*head = (*head)->next;
+		previous->next = NULL;
+	}
+	while (*head != NULL)
+	{
+		*head = (*head)->next;
 		current->next = previous;
 		previous = current;
-		current = next;
+		current = *head;
 	}
 	*head = previous;
 	return (*head);
