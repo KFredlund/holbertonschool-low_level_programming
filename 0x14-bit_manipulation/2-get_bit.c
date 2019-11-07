b97043fd11fcd3b831fcca69b563cce8f7775ee7
KFredlund <katihomes@gmail.com>
2019-11-07 22:45:47 +0000

    Create a function that returns the nth bit of a number

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* get_bit - Function that returns the value of a bit
+* at a given index
+* @n: int var
+* @index: index of the bit, starting from 0
+*
+* Return: the value of the bit at @index
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit;
+
+	bit = (n >> index) & 1;
+	return (bit);
+}
