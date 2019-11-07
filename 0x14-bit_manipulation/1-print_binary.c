e0574fdc0aeccf757f46b1d6d50b6396e1cf50e2
KFredlund <katihomes@gmail.com>
2019-11-07 22:33:24 +0000

    Create a function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* print_binary - A function that prints the binary
+* representation of a number
+* @n: number to be converted to binary
+*
+* Return: Never
+*/
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n / 2);
+	printf("%lu", n % 2);
+}
