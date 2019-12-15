db044ae2588458c0331fabdc29f7ccdc0fd1949f
KFredlund <katihomes@gmail.com>
2019-12-15 18:05:54 +0000

    Create a function that frees the elements of a linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+extern void free (void *__ptr) __THROW;
+/**
+* free_dlistint - A function that frees a doubly linked list
+* @head: points to the beginning of the list
+*
+* Return: never
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	current = malloc(sizeof(dlistint_t));
+	while (head)
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
+}
